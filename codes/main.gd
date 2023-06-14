e1329ca7cfab1d5f931b932a320d12fdb2f7ebe4
noosabaktee <ramanusabakti03@gmail.com>
2023-06-14 16:03:00 +0700

    Solved A button

--- codes/main.gd
+++ codes/main.gd
@@ -17,13 +17,6 @@ var closeDown = false
 func _ready():
 	window_y = get_viewport().size.y
 	window_x = get_viewport().size.x
-	arrow.visible = false
-	button.visible = false
-	
-	if(window_x < 550):
-		arrow.visible = true
-		button.visible = true
-		message.visible = false
 	
 	message.rect_position.y = window_y + 200
 	ballonAnim.play("Ballon Anim");
@@ -32,28 +25,14 @@ func _ready():
 func _on_viewport_size_changed():
 	window_y = get_viewport().size.y
 	window_x = get_viewport().size.x
-	if(window_x < 550):
-		arrow.visible = true
-		button.visible = true
-		message.visible = false
-	else:
-		arrow.visible = false
-		button.visible = false
-		message.visible = true
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if message.rect_position.y >= (window_y - 160) and closeDown == false:
+	if message.rect_position.y >= (window_y - 170) and closeDown == false:
 		message.rect_position -= Vector2(0,3)
-	if closeDown == true and message.rect_position.y <= 650:
+	if closeDown == true and message.rect_position.y <= (window_y + 100):
 		message.rect_position += Vector2(0,3)
 
-
-func _on_Close_button_down():
+func _on_close_pressed():
 	closeDown = true
-
-
-
-
-
+	print("click")
